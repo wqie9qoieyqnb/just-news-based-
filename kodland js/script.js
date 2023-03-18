24c503c4aa99f4573615f64c08e2a73d6e17652b
wqie9qoieyqnb <128241302+wqie9qoieyqnb@users.noreply.github.com>
2023-03-18 18:51:15 +0300

    Add files via upload
    my first commit

---
+++ kodland js/script.js
@@ -0,0 +1,15 @@
+let form = document.querySelector('.form');
+let inputs = document.querySelectorAll('.input');
+
+form.addEventListener("submit",function(e){
+e.preventDefault();
+inputs.forEach(function(item){
+
+
+
+})
+})
+
+
+
+setTimeout(func, 2000);
